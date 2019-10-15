0b766a739ca1d382780fe7d9043f6e0e431529b4
Lauren Riddle <lari7132@gmail.com>
2019-10-15 15:57:18 -0500

    edited the myParsedFoods function to generate food items in the DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,59 @@
-    const foodFactory = (food) => {
-       return `<div class="foodListItems"> 
+// const foodFactory = (food) => {
+//    return `<div class="foodListItems"> 
+//    <h1>${food.name}</h1>
+//     <section">${food.ethnicity} ${food.category}</section>
+//     </div>`
+// }
+
+// fetch("http://localhost:8088/food")
+
+
+// .then(foods => foods.json())
+
+// .then(parsedFoods => {
+//     parsedFoods.forEach(food => {
+//     console.log(foodFactory(food))
+//     const foodContainer = document.querySelector(".foodList")
+//     foodContainer.innerHTML += foodFactory(food)
+//     })
+// })
+
+
+const foodFactory = (food) => {
+    return `<div class="foodListItems"> 
        <h1>${food.name}</h1>
-        <section">${food.category}</section>
-        <aside>${food.ethnicity}</aside>
+        <section">${food.ethnicity} ${food.category}</section>
         </div>`
-    }
+}
 
-    fetch("http://localhost:8088/food")
-    
 
-    .then(foods => foods.json())
+fetch("http://localhost:8088/food")
+    .then(response => response.json())
 
-    .then(parsedFoods => {
-        parsedFoods.forEach(food => {
-        console.log(foodFactory(food))
-        const foodContainer = document.querySelector(".foodList")
-        foodContainer.innerHTML += foodFactory(food)
+    .then(myParsedFoods => {
+        myParsedFoods.forEach(food => {
+            console.log(food) // Should have a `barcode` property
+
+            // Now fetch the food from the Food API
+           fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+                .then(response => response.json())
+                .then(productInfo => {
+                    if (productInfo.product.ingredients_text) {
+                        food.ingredients = productInfo.product.ingredients_text
+                    } else {
+                        food.ingredients = "no ingredients listed"
+                    }
+
+                    // // Produce HTML representation
+                    const foodAsHTML = foodFactory(food)
+
+                    // // Add representaiton to DOM
+                    // addFoodToDom(foodAsHTML)
+                    const foodContainer = document.querySelector(".foodList")
+                    foodContainer.innerHTML += foodAsHTML
+                })
+
         })
-    })
 
+    })
+    
